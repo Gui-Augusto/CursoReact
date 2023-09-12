3722cdb826adad20f81f4f9f9af4749550f0d4e1
Guilherme <guilhermeaugustomarinho1@gmail.com>
2023-09-12 16:36:06 -0300

    feat: add login page and form

--- CursoReact/src/app/pages/login/Login.tsx
+++ CursoReact/src/app/pages/login/Login.tsx
@@ -1,8 +1,12 @@
+import { useState } from "react"
 
 export const Login = () => {
+    const[email, setEmail] = useState('')
+    const[senha, setSenha] = useState('')
 
     const handleEntrar = () => {
-
+        console.log(email)
+        console.log(senha)
     }
    
     return(
@@ -10,12 +14,12 @@ export const Login = () => {
             <form>
                 <label>
                     <span>Email</span>
-                    <input/>
+                    <input value={email} onChange={e => setEmail(e.target.value)}/>
                 </label>
 
                 <label>
                     <span>Senha</span>
-                    <input type="password"/>
+                    <input type="password" value={senha} onChange={e => setSenha(e.target.value)}/>
                 </label>
 
                 <button type="button" onClick={handleEntrar}>
